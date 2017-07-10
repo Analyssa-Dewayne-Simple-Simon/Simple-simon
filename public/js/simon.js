fa2ab972d3c62c23bcc1e7fa413e59b1801a38ca
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-07-10 15:40:07 -0500

    Worked on start button and got the color generator flashing colors

--- public/js/simon.js
+++ public/js/simon.js
@@ -1,5 +1,84 @@
 (function(){
 	"use strict";
 
+var colorBoxes = ["#red", "#blue", "#green", "#yellow"];
+var usedColors = [];
+
+
+function random(){
+	var randomColor = Math.floor(Math.random() * colorBoxes.length);
+	usedColors.push(colorBoxes[randomColor]);
+	return usedColors;
+};
+
+function flashColors(){
+	usedColors.forEach(function(element,index){
+		$(element).animate({
+			opacity: "1",	
+		}, 1000).animate({
+			opacity: "0.5"
+		}, 1000);
+	});
+		console.log(usedColors);
+}
+
+$("#start").click(function () {
+	random();
+	flashColors();
+});
+
+/* 
+1. Click start button
+ -display current round 
+ -generate the 1st random color box
+
+function startGame(){
+	var colorBoxes = [red, blue, green, yellow];
+ 	var randomColor = Math.random(colorBoxes.length);
+
+function randomSequence(){
+	return randomColor;
+	}
+
+ 2. Dislay the 1st random color box in bright color
+ and then opacity back to 0.5
+
+ 3. User clicks
+
+ 4. Check if user click = random color box that was
+ generated
+
+ 5. After 1st round,
+ cycle back through the function, show the 1st box
+ and then generate a random 2nd color box
+
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 })();
